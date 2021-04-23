15e354999ba22e7029cb48e72a181ad4c3099867
chen-sm1 <2973011320@qq.com>
2021-04-23 10:07:11 +0800

    Test

---
+++ 输出百分数.py
@@ -0,0 +1,29 @@
+﻿#utf-8
+#用格式控制符输出
+'''while 1:
+    x=input('输入一个数字')
+    print(x,end='%\n')
+'''
+
+'''
+while 1:
+    x=input('输入一个数字')
+    a=int(x)
+    b='%'
+    print(a,b)'''
+
+'''
+while 1:
+    x=input('输入一个数字')
+    print(x,'%')'''
+
+'''
+while 1:
+    x=input('输入一个数字') 
+    print(f'{x}%')
+    '''
+    
+while 1:
+    x=input('输入一个数字')
+    y=int(x)
+    print('%(a)d %(b)s'%{'a':y,'b':'%'})
